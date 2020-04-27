d42de199cecfd0490b803c2d05e369017356295a
Lisha Payne <lmp540@nyu.edu>
2020-04-26 20:44:56 -0400

    callies code

--- final-lookback/script.js
+++ final-lookback/script.js
@@ -1,53 +1,68 @@
 var app = {
-    initialize: function () {
-        app.getToken();
-    },
 
-    getToken: function() {
-        var id = '773495bcb32e481793e8419ec2eafc25';
+	//A place to set up listeners or kick off an initial function
+	initialize: function() {
+		//Start the auth procuess by using my id and secret to get an access token
+		app.getToken();
+	},
+
+	//This first ajax call uses my id and secret that I obtained from spotify to get
+	//an access token that is good for one hour
+	getToken: function(word) {
+		var id = '773495bcb32e481793e8419ec2eafc25';
         var secret = 'd999f7840a2d4a1caf9f23df16486fbd';
+		//this base encodes your id and secret to pass to the spotify server
+		var encoded = btoa(id + ':' + secret);
+		$.ajax(
+		  {
+		    method: "POST",
+		    url: "https://accounts.spotify.com/api/token",
+		    headers: {
+		    	'Authorization': 'Basic ' +  encoded
+		    },
+		    data: {
+		    	'grant_type': 'client_credentials'
+		    },
+		    success: function(result) {
+		    	//after I get my access token, I use it to search for a spotify track
+		    	app.searchTracks(result.access_token)
+		    },
+		  }
+		);
 
-        var encoded = btoa(id+':'+secret);
-      
-        $.ajax({
-            method: "POST",
-            url: "https://accounts.spotify.com/api/token",
-            headers: {
-                'Authorization': 'Basic' + encoded
-            },
-            
-            data: {
-                'grant-type': 'client_credentials'
-            },
-            
-            success: function(result){
-                console.log("sucessful");
-                app.searchTime(result.access_token)
-            },
+		
+	},
 
-            error: function(err){
-                console.log('error:' + err)
-            },
-        });
-    },
+	//This is the search to spotify's track api, with the token in an authorization header
+	searchTracks: function(token) {
+		 $.ajax({
+        url: "https://api.spotify.com/v1/tracks/7CZdd0S4WTktuiiARS8pY4",
+        dataType: "json",
+        headers: {
+					"Content-Type": "application/json",
+					"Authorization": "Bearer " + token
+				},
+        success: function(data) {
+            var trackName = data.name;
+            var url = data.preview_url;
 
-    searchTime: function(token){
-        $ajax({
-            url="https://api.spotify.com/v1/me/top/{type}",
-            dataType: "json",
-            headers: {
-                "Content-Type": "application/json",
-                "Authorization": "Bearer" + token
-            },
 
-            success: function(data){
+            var audioElement = document.createElement('audio');
+            audioElement.src = data.preview_url;
+            audioElement.autoplay = 'true';
+            audioElement.controls = 'true';
 
-            },
+            $('.title').html(trackName);
+            $('.the-track').html(audioElement);
+           
+        },
 
-            error: function(err){
-                console.log('error:' + err)
-            },
+        error: function() {
+          console.log("Error retrieving spotify API");
+        }
+	    });
 
-        });
-    },
-} 
+	}
+
+
+};
