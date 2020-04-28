354fdc4d41a246dc0b701cbb58e5a2f89b899d6e
Lisha Payne <lmp540@nyu.edu>
2020-04-27 21:09:14 -0400

    spotify api done?

--- final-lookback/script.js
+++ final-lookback/script.js
@@ -1,68 +1,87 @@
 var app = {
-
-	//A place to set up listeners or kick off an initial function
 	initialize: function() {
-		//Start the auth procuess by using my id and secret to get an access token
-		app.getToken();
+		app.requestAuthorization();
 	},
 
-	//This first ajax call uses my id and secret that I obtained from spotify to get
-	//an access token that is good for one hour
-	getToken: function(word) {
+	requestAuthorization: function(word){
 		var id = '773495bcb32e481793e8419ec2eafc25';
-        var secret = 'd999f7840a2d4a1caf9f23df16486fbd';
-		//this base encodes your id and secret to pass to the spotify server
-		var encoded = btoa(id + ':' + secret);
-		$.ajax(
-		  {
-		    method: "POST",
-		    url: "https://accounts.spotify.com/api/token",
-		    headers: {
-		    	'Authorization': 'Basic ' +  encoded
-		    },
-		    data: {
-		    	'grant_type': 'client_credentials'
-		    },
-		    success: function(result) {
-		    	//after I get my access token, I use it to search for a spotify track
-		    	app.searchTracks(result.access_token)
-		    },
-		  }
-		);
+		var scope = 'user-read-recently-played';
+		var response_type = 'code';
+		var redirect_uri = 'https%3A%2F%2Fgithub.com%2Flishap%2FIMA-frontEnd%2Ftree%2Fmaster%2Ffinal-lookback';
 
-		
+		$.ajax({
+			method: "GET",
+			url: "https://accounts.spotify.com/authorize",
+			headers: {
+				client_id: id,
+				response_type: response_type,
+				redirect_uri: redirect_uri,
+				scope: scope,
+			},
+			
+			success: function(result) {
+		    	app.requestRefreshActionTokens(result)
+		    },
+		})
 	},
 
-	//This is the search to spotify's track api, with the token in an authorization header
-	searchTracks: function(token) {
-		 $.ajax({
-        url: "https://api.spotify.com/v1/tracks/7CZdd0S4WTktuiiARS8pY4",
-        dataType: "json",
-        headers: {
-					"Content-Type": "application/json",
-					"Authorization": "Bearer " + token
-				},
-        success: function(data) {
-            var trackName = data.name;
-            var url = data.preview_url;
+	requestRefreshActionTokens: function(word){
+		var id = '773495bcb32e481793e8419ec2eafc25';
+		var encoded = btoa(id + ':' + secret);
+		
+		$.ajax({
+			method: "POST",
+			url: "https://accounts.spotify.com/api/token",
+			headers: {
+				'Authorization': 'Basic ' +  encoded
+			},
+			data: {
+				'grant-type': 'authorization_code',
+				'code': result,
+				'redirect_uri': redirect_uri,
+			},
 
+			success: function(result) {
+		    	app.accessSpotifyWebAPI(result)
+		    },
+		})
+	},
 
-            var audioElement = document.createElement('audio');
-            audioElement.src = data.preview_url;
-            audioElement.autoplay = 'true';
-            audioElement.controls = 'true';
+	accessSpotifyWebAPI: function(word){
+		$.ajax({
+			method: "POST",
+			url: "https://api.spotify.com/v1/me",
+			headers: {
+				'Authorization':'Bearer' + result
+			},
 
-            $('.title').html(trackName);
-            $('.the-track').html(audioElement);
-           
-        },
+			success: function(result) {
+		    	app.requestRefreshAccessToken(result)
+		    },
+		})
+	},
 
-        error: function() {
-          console.log("Error retrieving spotify API");
-        }
-	    });
-
+	requestRefreshAccessToken: function(word){
+		$.ajax({
+			method: "POST",
+			url: "https://accounts.spotify.com/api/token",
+			headers: {
+				'Authorization': 'Basic' + encoded
+			},
+			data: {
+				'grant_type':'refresh_token',
+				'refresh_token': result
+			},
+		})
 	}
 
+	
 
-};
+
+
+
+
+
+
+}
+
